7cafa9da131373d0fb83b394f65bc16fb6711fae
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 12:08:25 +0530

    add help

--- az-cli-scripts/webapp/create.sh
+++ az-cli-scripts/webapp/create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if["$1" == "-h"]; then
-  echo "appserviceplan takes $1 : resource-group; $2 : name"
-  echo "webapp takes $1 : resource-group; $2 : name; $3 : appserviceplan name"
-exit 0
+ echo "appserviceplan takes $1 : resource-group; $2 : name"
+ echo "webapp takes $1 : resource-group; $2 : name; $3 : appserviceplan name"
+ exit 0
 fi
 
 #create a appservice plan in free tier ($1: resource group, $2: plan name, sku is F1)
--- az-cli-scripts/webapp/delete.sh
+++ az-cli-scripts/webapp/delete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if["$1" == "-h"]; then
+if[ "$1" == "-h" ]; then
   echo "appserviceplan takes $1 : resource-group; $2 : name"
   echo "webapp takes $1 : resource-group; $2 : name; $3 : appserviceplan"
-exit 0
+  exit 0
 fi
 #delete a appservice plan in free tier ($1: resource group, $2: plan name, sku is F1)
 az appservice plan delete --resource-group "rg-$1" --name "$2" --sku F1
