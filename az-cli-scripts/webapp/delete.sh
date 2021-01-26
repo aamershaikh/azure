fe4a56450e8c02f22e1c7dc7406c36ca203e53d0
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 12:37:24 +0530

    change

--- az-cli-scripts/webapp/delete.sh
+++ az-cli-scripts/webapp/delete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #delete a appservice plan in free tier ($1: resource group, $2: plan name, sku is F1)
-az appservice plan delete --resource-group "rg-rg1" --name "myappserviceplan"
+az appservice plan delete --resource-group "rg-rg1" --name "rg1-myappserviceplan"
 
 #delete a webapp
-az webapp delete --resource-group "rg-rg1" --name "mywebapp"  --plan "myappserviceplan"
+az webapp delete --resource-group "rg-rg1" --name "rg1-mywebapp"  --plan "rg1-myappserviceplan"
