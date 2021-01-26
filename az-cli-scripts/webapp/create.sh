e389dc2e33a5f4ff955d0ecc393b2dfe3c8b435f
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 12:04:22 +0530

    add help

--- az-cli-scripts/webapp/create.sh
+++ az-cli-scripts/webapp/create.sh
@@ -3,6 +3,8 @@
 if["$1" == "-h"]; then
   echo "appserviceplan takes $1 : resource-group; $2 : name"
   echo "webapp takes $1 : resource-group; $2 : name; $3 : appserviceplan name"
+exit 0
+fi
 
 #create a appservice plan in free tier ($1: resource group, $2: plan name, sku is F1)
 az appservice plan create --resource-group "$1" --name "$2" --sku F1
