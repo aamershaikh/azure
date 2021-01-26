c1c3f886eeccfb70d9d1de4aca246ac0e7675fa7
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 17:30:40 +0530

    web app delete scrip

--- az-cli-scripts/webapp/delete.sh
+++ az-cli-scripts/webapp/delete.sh
@@ -4,4 +4,4 @@
 az appservice plan delete --resource-group "rg-rg1" --name "rg1-myappserviceplan"
 
 #delete a webapp
-az webapp delete --resource-group "rg-rg1" --name "rg1-mywebapp"  --plan "rg1-myappserviceplan"
+az webapp delete --resource-group "rg-rg1" --name "rg1-mywebapp"
