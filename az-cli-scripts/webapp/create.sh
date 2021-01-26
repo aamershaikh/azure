3315347858833faaac04c087a5a09d6f6d3d45aa
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 12:30:21 +0530

    removed sku

--- az-cli-scripts/webapp/create.sh
+++ az-cli-scripts/webapp/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #create a appservice plan in free tier ($1: resource group, $2: plan name, sku is F1)
-az appservice plan create --resource-group "rg-rg1" --name "myappserviceplan" --sku F1
+az appservice plan create --resource-group "rg-rg1" --name "myappserviceplan"
 
 #create a webapp
 az webapp create --resource-group "rg-rg1" --name "mywebapp" --plan "myappserviceplan"
