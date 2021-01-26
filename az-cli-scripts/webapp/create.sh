f474169df531bbc1c3181afc239e50819d33d08a
aamershaikh <aameriftekhar@gmail.com>
2021-01-26 12:10:42 +0530

    add help

--- az-cli-scripts/webapp/create.sh
+++ az-cli-scripts/webapp/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if["$1" == "-h"]; then
+if["$1" == "-h"];
  echo "appserviceplan takes $1 : resource-group; $2 : name"
  echo "webapp takes $1 : resource-group; $2 : name; $3 : appserviceplan name"
  exit 0
